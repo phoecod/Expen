19d80668f29124895295ffbdb6fcc51a168db5dd
phoecod <ragyyriad@hotmail.com>
2018-07-27 21:11:50 -0400

    remove dummy data from app.js

--- src/app.js
+++ src/app.js
@@ -14,33 +14,7 @@ import 'react-dates/lib/css/_datepicker.css';
 const store = configureStore;
 store.subscribe(() => {
 	const state = store.getState();
-	console.log(state);
 });
-
-const expOne = {
-	amount: 10000,
-	description: 'Gas Bill',
-	note: 'how clearer can I be',
-	createdAt: moment().set({'year': 2018, 'month': 5, 'date': 4})
-}
-
-const expTwo = {
-	amount: 1200,
-	description: 'phone Bill',
-	note: 'Hello?',
-	createdAt: moment().set({'year': 2018, 'month': 5, 'date': 24})
-}
-
-const expThree = {
-	amount: 12000,
-	description: 'shoes',
-	createdAt: moment().set({'year': 2010, 'month': 6, 'date': 26})
-}
-
-
-store.dispatch(addExpense(expOne));
-store.dispatch(addExpense(expTwo));
-store.dispatch(addExpense(expThree));
 
 const jsx = (
 	<Provider store={store}>
